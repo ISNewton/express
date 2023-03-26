587779b309b91fe5b83ed843568af0b9abb026e9
ISNewton <ashrfalhag.11@gmail.com>
2023-03-26 22:02:05 +0200

    add 404 page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ const shopRoutes = require('./shop')
 app.use(adminRoutes)
 app.use(shopRoutes)
 
+app.use((req , res , next) => {
+    res.status(404).send('<h1>404 , Page not found</h1>')
+})
+
 module.exports = app
