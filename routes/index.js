04bd0f2ec8aeaf9e66ce268c75a0e8651490179a
ISNewton <ashrfalhag.11@gmail.com>
2023-03-26 22:24:46 +0200

    add 404 page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const app = require('express')()
 
+const path = require('path')
+
 const adminRoutes = require('./admin')
 const shopRoutes = require('./shop')
 
@@ -7,7 +9,7 @@ app.use('/admin' , adminRoutes)
 app.use(shopRoutes)
 
 app.use((req , res , next) => {
-    res.status(404).send('<h1>404 , Page not found</h1>')
+    res.sendFile(path.join(__dirname , '../' , 'views' , '404.html'))
 })
 
 module.exports = app
