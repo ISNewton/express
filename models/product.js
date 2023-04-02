1c63f97d83b2c07d31e39947756854fd7e9763f4
ISNewton <ashrfalhag.11@gmail.com>
2023-04-02 19:21:46 +0200

    update product model to use sequelize

--- models/product.js
+++ models/product.js
@@ -1,112 +1,126 @@
-const fs = require('fs');
-const path = require('path');
-const db = require('../util/database')
+const Sequelize = require('sequelize')
+const sequelize = require('../util/database')
 
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
-
-module.exports = class Product {
-  constructor(title, image, description, price, id = null) {
-    this.title = title;
-    this.image = image;
-    this.description = description;
-    this.price = price;
-
-    if (id) {
-      this.id = id
-    }
-  }
-
-  save() {
-    // products.push(this);
-    // fs.writeFile(p, JSON.stringify(products), err => {
-    // });
-
-    db.execute('insert into products (title,image,price,description) values(?,?,?,?)',
-      [this.title, this.image, this.price, this.description]
-    )
-      .then(result => console.log(result))
-      .catch(err => console.log(err))
-  }
-
-  delete() {
-    // getProductsFromFile(products => {
-
-    //   const newProducts = products.filter(product => product.id != this.id)
-
-    //   fs.writeFile(p, JSON.stringify(newProducts), err => {
-    //   });
-
-    // })
-
-    db.execute('delete from products where id = ?', [this.id])
-      .then(result => console.log(result))
-      .catch(err => console.log(err))
-  }
-
-  static fetchAll(cb) {
-    // getProductsFromFile(cb);
-
-    db.execute('select * from products')
-      .then(([products, fieldData]) => {
-        cb(products)
-      })
-  }
-
-  static find(id, cb) {
-    // return getProductsFromFile((products) => {
-    //   cb(products.find(product => product.id == id))
-    // })
-
-    db.execute('select * from products where id = ? limit 1', [id])
-      .then(([products, fieldData]) => {
-        cb(products[0])
-      })
-
-  }
-
-  update(product) {
-    // getProductsFromFile((products) => {
-    //   const index = products.findIndex(pro => pro.id == this.id)
-
-    //   products[index] = {
-    //     ...product,
-    //     id: this.id
-    //   }
-    //   fs.writeFile(p, JSON.stringify(products), err => {
-    //   });
-
-    // })
-    let fieldsAndValues = ''
-
-    for (const key in product) {
-      const element = product[key];
-      fieldsAndValues += ` ${key} = ?,`
-
-    }
-
-    fieldsAndValues = fieldsAndValues.substring(0 , fieldsAndValues.length - 1)
+module.exports = sequelize.define('product',{
+  id: {
+    type: Sequelize.BIGINT,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  title: Sequelize.STRING,
+  price: Sequelize.FLOAT,
+  image: Sequelize.STRING,
+  description: Sequelize.TEXT,
+})
 
 
-    // console.log(fieldsAndValues);
-    // db.execute(`update products set title = ? where id = 5`, ['title'])
-    db.execute(`update products set ${fieldsAndValues} where id = ?`, [...Object.values(product),this.id])
-    .then(([products, fieldData]) => {
-      // cb(products)
-    })
-  }
-};
+
+// const p = path.join(
+//   path.dirname(process.mainModule.filename),
+//   'data',
+//   'products.json'
+// );
+
+// const getProductsFromFile = cb => {
+//   fs.readFile(p, (err, fileContent) => {
+//     if (err) {
+//       cb([]);
+//     } else {
+//       cb(JSON.parse(fileContent));
+//     }
+//   });
+// };
+
+// module.exports = class Product {
+//   constructor(title, image, description, price, id = null) {
+//     this.title = title;
+//     this.image = image;
+//     this.description = description;
+//     this.price = price;
+
+//     if (id) {
+//       this.id = id
+//     }
+//   }
+
+//   save() {
+//     // products.push(this);
+//     // fs.writeFile(p, JSON.stringify(products), err => {
+//     // });
+
+//     db.execute('insert into products (title,image,price,description) values(?,?,?,?)',
+//       [this.title, this.image, this.price, this.description]
+//     )
+//       .then(result => console.log(result))
+//       .catch(err => console.log(err))
+//   }
+
+//   delete() {
+//     // getProductsFromFile(products => {
+
+//     //   const newProducts = products.filter(product => product.id != this.id)
+
+//     //   fs.writeFile(p, JSON.stringify(newProducts), err => {
+//     //   });
+
+//     // })
+
+//     db.execute('delete from products where id = ?', [this.id])
+//       .then(result => console.log(result))
+//       .catch(err => console.log(err))
+//   }
+
+//   static fetchAll(cb) {
+//     // getProductsFromFile(cb);
+
+//     db.execute('select * from products')
+//       .then(([products, fieldData]) => {
+//         cb(products)
+//       })
+//   }
+
+//   static find(id, cb) {
+//     // return getProductsFromFile((products) => {
+//     //   cb(products.find(product => product.id == id))
+//     // })
+
+//     db.execute('select * from products where id = ? limit 1', [id])
+//       .then(([products, fieldData]) => {
+//         cb(products[0])
+//       })
+
+//   }
+
+//   update(product) {
+//     // getProductsFromFile((products) => {
+//     //   const index = products.findIndex(pro => pro.id == this.id)
+
+//     //   products[index] = {
+//     //     ...product,
+//     //     id: this.id
+//     //   }
+//     //   fs.writeFile(p, JSON.stringify(products), err => {
+//     //   });
+
+//     // })
+//     let fieldsAndValues = ''
+
+//     for (const key in product) {
+//       const element = product[key];
+//       fieldsAndValues += ` ${key} = ?,`
+
+//     }
+
+//     fieldsAndValues = fieldsAndValues.substring(0 , fieldsAndValues.length - 1)
+
+
+//     // console.log(fieldsAndValues);
+//     // db.execute(`update products set title = ? where id = 5`, ['title'])
+//     db.execute(`update products set ${fieldsAndValues} where id = ?`, [...Object.values(product),this.id])
+//     .then(([products, fieldData]) => {
+//       // cb(products)
+//     })
+//   }
+// };
